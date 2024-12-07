c56e92ad1c78c74d5a082e2ef6bde101fb1d5138
syrok32 <stepstepan2@gmail.com>
2024-12-07 14:10:18 +0300

    migrate 1

--- Lesson2/settings.py
+++ Lesson2/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,11 +77,16 @@ WSGI_APPLICATION = 'Lesson2.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+load_dotenv()
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT', default='5432'),
+
     }
 }
 
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=25, verbose_name='название категории')
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = ('категории')
+        ordering = ['name']
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, verbose_name='имя')
+    description = models.CharField(max_length=200, verbose_name='описание')
+    image = models.ImageField(upload_to='images/', blank=True, null=True, verbose_name='фото')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='ПРОДУКТЫ')
+    price = models.IntegerField(verbose_name='цена')
+    created_at = models.DateField()
+    updated_at = models.DateField()
+
+    def __str__(self):
+        return f'{self.name}{self.price}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ['name']
